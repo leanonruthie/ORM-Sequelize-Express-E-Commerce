74a0be27b9eeeefbea6b35075177adae8e76a2d3
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-09-18 18:17:22 -0400

    easy repair for server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+
+
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+// WORK REFERENCE - basically all activities inside - RUT-VIRT-FSF-PT-06-2022-U-LOLC/13-ORM/01-Activities - indicate below
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// WORK REFERENCE - basically all activities inside - RUT-VIRT-FSF-PT-06-2022-U-LOLC/13-ORM/01-Activities - indicate below
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
