4281253dc9392fbcfc056f9b9d908f5ac84cbf82
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-09-18 19:14:19 -0400

    clean slate for routes folder since i will work on models folder first

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,27 +7,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.findAll().then((bookData) => {
-    res.json(bookData);
-  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.findOne(
-    {
-      // Gets the book based on the isbn given in the request parameters
-      where: { 
-        isbn: req.params.isbn 
-      },
-    }
-  ).then((bookData) => {
-    res.json(bookData);
-  });
 });
 
 // create new product
@@ -40,62 +25,6 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-// reference found in 01-Activities/07-Ins_Update-Delete
-  Book.bulkCreate([
-    {
-      title: 'Make It Stick: The Science of Successful Learning',
-      author: 'Peter Brown',
-      isbn: '9780674729018',
-      pages: 336,
-      edition: 1,
-      is_paperback: false,
-    },
-    {
-      title:
-        'Essential Scrum: A Practical Guide to the Most Popular Agile Process',
-      author: 'Kenneth Rubin',
-      isbn: '9780137043293',
-      pages: 500,
-      edition: 1,
-      is_paperback: true,
-    },
-    {
-      title:
-        "White Fragility: Why It's So Hard for White People to Talk About Racism",
-      author: 'Robin DiAngelo',
-      isbn: '9780807047415',
-      pages: 192,
-      edition: 2,
-      is_paperback: true,
-    },
-    {
-      title: 'The Pragmatic Programmer: Your Journey To Mastery',
-      author: 'David Thomas',
-      isbn: '9780135957059',
-      pages: 352,
-      edition: 2,
-      is_paperback: false,
-    },
-    {
-      title: 'The Art of Computer Programming, Vol. 1: Fundamental Algorithms',
-      author: 'Donald Knuth',
-      isbn: '9780201896831',
-      pages: 672,
-      edition: 3,
-      is_paperback: false,
-    },
-    {
-      title: 'Algorithms of Oppression: How Search Engines Reinforce Racism',
-      author: 'Safiya Umoja Noble',
-      isbn: '9781479837243',
-      pages: 256,
-      edition: 1,
-      is_paperback: true,
-    },
-  ]).then(() => {
-    res.send('Seeding Success!');
-  });
-
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,126 +6,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.findAll().then((bookData) => {
-    res.json(bookData);
-  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.findOne(
-    {
-      // Gets the book based on the isbn given in the request parameters
-      where: { 
-        isbn: req.params.isbn 
-      },
-    }
-  ).then((bookData) => {
-    res.json(bookData);
-  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.bulkCreate([
-    {
-      title: 'Make It Stick: The Science of Successful Learning',
-      author: 'Peter Brown',
-      isbn: '9780674729018',
-      pages: 336,
-      edition: 1,
-      is_paperback: false,
-    },
-    {
-      title:
-        'Essential Scrum: A Practical Guide to the Most Popular Agile Process',
-      author: 'Kenneth Rubin',
-      isbn: '9780137043293',
-      pages: 500,
-      edition: 1,
-      is_paperback: true,
-    },
-    {
-      title:
-        "White Fragility: Why It's So Hard for White People to Talk About Racism",
-      author: 'Robin DiAngelo',
-      isbn: '9780807047415',
-      pages: 192,
-      edition: 2,
-      is_paperback: true,
-    },
-    {
-      title: 'The Pragmatic Programmer: Your Journey To Mastery',
-      author: 'David Thomas',
-      isbn: '9780135957059',
-      pages: 352,
-      edition: 2,
-      is_paperback: false,
-    },
-    {
-      title: 'The Art of Computer Programming, Vol. 1: Fundamental Algorithms',
-      author: 'Donald Knuth',
-      isbn: '9780201896831',
-      pages: 672,
-      edition: 3,
-      is_paperback: false,
-    },
-    {
-      title: 'Algorithms of Oppression: How Search Engines Reinforce Racism',
-      author: 'Safiya Umoja Noble',
-      isbn: '9781479837243',
-      pages: 256,
-      edition: 1,
-      is_paperback: true,
-    },
-  ]).then(() => {
-    res.send('Seeding Success!');
-  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  // reference found in 01-Activities/07-Ins_Update-Delete
-Book.update(
-  {
-    // All the fields you can update and the data attached to the request body.
-    title: req.body.title,
-    author: req.body.author,
-    isbn: req.body.isbn,
-    pages: req.body.pages,
-    edition: req.body.edition,
-    is_paperback: req.body.is_paperback,
-  },
-  {
-    // Gets the books based on the isbn given in the request parameters
-    where: {
-      isbn: req.params.isbn,
-    },
-  }
-)
-  .then((updatedBook) => {
-    // Sends the updated book as a json response
-    res.json(updatedBook);
-  })
-  .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  // reference found in 01-Activities/07-Ins_Update-Delete
-  Book.destroy({
-    where: {
-      isbn: req.params.isbn,
-    },
-  })
-    .then((deletedBook) => {
-      res.json(deletedBook);
-    })
-    .catch((err) => res.json(err));
 });
 
 module.exports = router;
